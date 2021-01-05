7a8a7510ecf172e4736972484b8de22f1a98b81d
yakov-work <yakovgulyuta@yandex.ru>
2021-01-05 23:49:04 +0300

    добавил postgre

--- symfony/public/index.php
+++ symfony/public/index.php
@@ -1,2 +1,10 @@
 <?php
-echo phpinfo();
+
+$db = new PDO('pgsql:dbname=test;host=lesson-postgres', 'yakov', 123);
+$query = "SELECT table_name FROM information_schema.tables WHERE table_schema='public'";
+$statement = $db->prepare($query);
+$bool = $statement->execute();
+$result = $statement->fetchAll(2);
+echo '<pre>';
+print_r($result);
+echo '<pre>';
